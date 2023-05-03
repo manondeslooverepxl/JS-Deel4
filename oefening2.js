71499cfd74a44703fe51e99227c01b9b0d72c4da
manondeslooverepxl <manon.desloovere@student.pxl.be>
2023-05-03 10:33:20 +0200

    les 3/05

---
+++ oefening2.js
@@ -0,0 +1,11 @@
+// font van hele webpagina
+document.getElementsByTagName("body")[0].style.fontFamily = "Arial, Verdana, sans-serif";
+
+// titelkleur + onderlijning
+document.getElementsByTagName("h1")[0].style.color = "green";
+document.getElementsByTagName("h1")[0].style.textDecoration = "underline";
+
+// naam vet + schuin
+document.getElementsByTagName("p")[0].style.fontWeight = "bold";
+document.getElementsByTagName("p")[0].style.fontStyle = "italic";
+
---
+++ oefening3.js
@@ -0,0 +1,22 @@
+// tekstkleur paragraaf
+document.getElementById("red").style.color = "red";
+
+// verwijder paragraaf
+
+document.getElementById("weg").remove();
+
+// hoofding toevoegen
+let paragraaf = document.createElement("h1");
+let paragraafTekst = document.createTextNode("Welkom PandiX");
+let heading = document.getElementById("header");
+heading.appendChild(paragraaf);
+paragraaf.appendChild(paragraafTekst);
+
+// aside toevoegen
+let adres = document.getElementById("main");
+let aside = document.createElement("aside");
+let asideP = document.createElement("p");
+let asideTekst = document.createTextNode("Elfde-Liniestraat 24");
+adres.appendChild(aside);
+aside.appendChild(asideP);
+asideP.appendChild(asideTekst);
